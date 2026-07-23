e4d0664d186faa709407c8a345f900f2ec5bed84
ganeshfaterpekar <ganeshfaterpekar@users.noreply.github.com>
2026-07-23 12:40:11 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.write(
   """
 )
 
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 st.text(smoothiefroot_response)
 # sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
